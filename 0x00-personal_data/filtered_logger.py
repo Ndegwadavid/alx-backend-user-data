f247440e700f37421e3cd990e4dc4c551e685f49
Ndegwadavid <davidndegwa6869@gmail.com>
2024-04-11 09:22:56 +0300

    Log message obfuscate

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+function that return log message obfuscated
+'''
+
+def filter_datum(fields, redaction, message, separator):
+    '''
+    function that return log message obfuscated
+    '''
+    for field in fields:
+        message = message.replace(field + separator, field + separator + redaction + separator)
+    return (message)
